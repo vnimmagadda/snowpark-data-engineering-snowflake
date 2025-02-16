397244a4b726f33e068a28f511d4a2bdba2c6ef1
VishnuKiranN <Vishnu.Kiran@ricoh-usa.com>
2025-02-15 21:39:55 -0500

    commit

---
+++ Snowpark_Dev/Connection_test.py
@@ -0,0 +1,19 @@
+import os
+import snowflake.snowpark.functions
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import col
+
+connection_parameters = {"account":"vbb48100.us-east-1",
+"user":"VKIRAN",
+"password": "Transform@24",
+"role":"ACCOUNTADMIN",
+"warehouse":"COMPUTE_WH",
+"database":"DEMO_DB",
+"schema":"PUBLIC"
+}
+
+test_session = Session.builder.configs(connection_parameters).create()
+
+print(test_session.sql("select current_warehouse(), current_database(), current_schema()").collect())
+
+session = Session.builder.configs(connection_parameters).create()
